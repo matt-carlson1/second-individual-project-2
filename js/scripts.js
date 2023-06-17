2ed0fdfd9505567f3ee9c78b4eaa6798b34950de
matt-carlson1 <cmatthew42@hotmail.com>
2023-06-17 13:16:55 -0700

    WIP second form

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,13 @@ window.onload = function () {
     }
   };
 };
+function handleRadio(event) {
+  event.preventDefault();
+  const radioSelection = document.querySelector(
+    "input[name='flavor']:checked"
+  ).value;
+}
+
+window.addEventListener("load", function () {
+  document.getElementById("radio-form").addEventListener("submit", handleRadio);
+});
