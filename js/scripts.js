4794a8cc82f5e594dd15c7b28a05ac757e69a094
matt-carlson1 <cmatthew42@hotmail.com>
2023-06-17 17:03:19 -0700

    Change font color for questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,3 @@
-window.onload = function () {
-  const form = document.querySelector("form");
-  form.onsubmit = function (event) {
-    event.preventDefault();
-
-    let voterInfo = document.getElementById("voter");
-    voterInfo.setAttribute("class", "hidden");
-
-    let under18Message = document.getElementById("under-18");
-    under18Message.setAttribute("class", "hidden");
-
-    const age = parseInt(document.querySelector("input#age").value);
-
-    if (age >= 18) {
-      voterInfo.removeAttribute("class");
-    } else {
-      under18Message.removeAttribute("class");
-    }
-  };
-};
 function handleRadio(event) {
   event.preventDefault();
   const radioSelection = document.querySelector(
