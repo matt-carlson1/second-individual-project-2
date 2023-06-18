5b561a053283073fdb2285997dc199d23d25dd99
matt-carlson1 <cmatthew42@hotmail.com>
2023-06-17 19:07:36 -0700

    WIP debug form

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,30 @@
 function handleRadio(event) {
   event.preventDefault();
-  const radioSelection = document.querySelector(
+  console.log("first test");
+  const endRadioSelection = document.querySelector(
     "input[name='end']:checked"
   ).value;
+  const basedRadioSelection = document.querySelector(
+    "input[name='based']:checked"
+  ).value;
+  const difficultyRadioSelection = document.querySelector(
+    "input[name='difficulty']:checked"
+  ).value;
+  const speedRadioSelection = document.querySelector(
+    "input[name='speed']:checked"
+  ).value;
+  const libraryRadioSelection = document.querySelector(
+    "input[name='library']:checked"
+  ).value;
+  console.log(endRadioSelection);
+  console.log(basedRadioSelection);
+  console.log(difficultyRadioSelection);
+  console.log(speedRadioSelection);
+  console.log(libraryRadioSelection);
 }
 
 window.addEventListener("load", function () {
+  console.log("testing");
   document.getElementById("radio-form").addEventListener("submit", handleRadio);
+  console.log("this is running");
 });
